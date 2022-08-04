975dbf72d7611b0bc210c66e8d414e3842aeb195
SN19Y-MRT <yuuki.karate1ban@icloud.com>
2022-08-04 14:40:42 +0900

    weathercodeを日本語に変換

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -39,17 +39,56 @@ class WeatherController extends Controller
 
         $weather = $response->getBody();
         $posts = json_decode($weather, true);
-        
         $data = [];
+        
         for($i = 0; $i <= 6; $i++) {
+            $weathercode = $posts['daily']['weathercode'][$i];
             $data[] = [
                 'time'=>$posts['daily']['time'][$i],
-                'weathercode'=>$posts['daily']['weathercode'][$i],
+                'weathername'=>$this->weathername($weathercode),
                 'temperature_2m_max'=>$posts['daily']['temperature_2m_max'][$i],
                 'temperature_2m_min'=>$posts['daily']['temperature_2m_min'][$i],
             ];
         }
         
+        
         return view('index', ['data' => $data]);
+    }
+    
+    public function weathername($weathercode)
+    {
+            if($weathercode == 0){
+                return '快晴';
+            }
+            if($weathercode == 1){
+                return '晴れ';
+            }
+            if($weathercode == 2){
+                return '一部雲';
+            }
+            if($weathercode == 3){
+                return '曇り';
+            }
+            if($weathercode <= 49){
+                return '霧';
+            }
+            if($weathercode <= 59){
+                return '霧雨';
+            }
+            if($weathercode <= 69){
+                return '雨';
+            }
+            if($weathercode <= 79){
+                return '雪';
+            }
+            if($weathercode <= 84){
+                return 'にわか雨';
+            }
+            if($weathercode <= 94){
+                return '雪・雹';
+            }
+            if($weathercode <= 99){
+                return '雷雨';    
+            }
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,21 +6,8 @@
       <!-- Fonts -->
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-      
-     
     </head>
-
-    <body>
-      <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-      <meta charset="utf-8">
-      <title>週間天気予報</title>
-      <!-- Fonts -->
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    </head>
-
+  
     <body>
       <table class="table">
         <thead>
@@ -32,25 +19,19 @@
             <th scope="col">最低気温</th>
           </tr>
         </thead>
-        
-        
-        
-        @foreach($data as $datas)
         <tbody>
+        @foreach($data as $datas)
           <tr>
             <td></td>
             <td>{{ $datas['time'] }}</td>
-            <td>{{ $datas['weathercode'] }}</td>
+            <td>{{ $datas['weathername'] }}</td>
             <td>{{ $datas['temperature_2m_max'] }}</td>
             <td>{{ $datas['temperature_2m_min'] }}</td>
           </tr>
-        </tbody>
         @endforeach
+        </tbody>
       </table>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-    </body>
-</html>
-
     </body>
 </html>
